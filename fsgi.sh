03aa7cba4a6233261ba81ef351680a2a096ecfe9
MaxOOOOON <lisok.maksim@gmail.com>
2021-08-10 22:19:10 +0300

    add

---
+++ fsgi.sh
@@ -0,0 +1,24 @@
+cat << EOF > /etc/sysconfig/spawn-fcgi
+SOCKET=/var/run/php-fcgi.sock
+OPTIONS="-u apache -g apache -s $SOCKET -S -M 0600 -C 32 -F 1 -P /var/run/spawn-fcgi.pid -- /usr/bin/php-cgi"
+EOF
+
+cat << EOF >> /etc/systemd/system/spawn-fcgi.service 
+[Unit]
+Description=Spawn-fcgi startup service by Otus
+After=network.target
+
+[Service]
+Type=simple
+PIDFile=/var/run/spawn-fcgi.pid
+EnvironmentFile=/etc/sysconfig/spawn-fcgi
+ExecStart=/usr/bin/spawn-fcgi -n $OPTIONS
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+systemsctl enable spawn-fcgi
+systemsctl start spawn-fcgi
